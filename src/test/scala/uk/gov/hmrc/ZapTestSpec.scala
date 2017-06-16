8d5422b9099c238435447272daf8f4494deef8f1
dawn-carvell <dawn.carvell@digital.cabinet-office.gov.uk>
2017-06-16 17:27:56 +0100

    finishes up a unit test around creating contexts

--- src/test/scala/uk/gov/hmrc/ZapTestSpec.scala
+++ src/test/scala/uk/gov/hmrc/ZapTestSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc
 
 import org.mockito.ArgumentMatchers.any
@@ -67,7 +83,8 @@ class ZapTestSpec extends FunSpec with Matchers with MockitoSugar {
     it("should return the context ID") {
       when(insecureClientMock.getRawResponse(any(), any())(any())).thenReturn((200, "{\n\"contextId\": \"2\"\n}"))
 
-      val context = zapTest.createContext()
+      val context: Context = zapTest.createContext()
+      context.id shouldBe("2")
     }
   }
 
