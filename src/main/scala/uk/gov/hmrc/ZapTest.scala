0282da42e0242fc5f922ea2e8fe590ec52f4d379
Dan Prokopiwskyi <dan.prokopiwskyi@digital.hmrc.gov.uk>
2018-05-24 16:00:04 +0100

    create a configuration class with all ZapTest configurable properties present.  Refactored ZapTest to use the instatiated ZapConfig, and not the typesafe Config.
    implemented a debug property to print the configuration

--- src/main/scala/uk/gov/hmrc/ZapTest.scala
+++ src/main/scala/uk/gov/hmrc/ZapTest.scala
@@ -18,36 +18,42 @@ package uk.gov.hmrc
 
 import java.io.{BufferedWriter, File, FileWriter}
 import java.util.UUID
-
-import com.typesafe.config.{Config, ConfigFactory, ConfigObject, ConfigValue}
+import com.typesafe.config.Config
 import org.scalatest.WordSpec
 import org.slf4j.Logger
 import play.api.libs.json._
-import uk.gov.hmrc.utils.{LoadConfig, TestHelper, WsClient, ZapLogger}
+import uk.gov.hmrc.utils.{ZapConfig, TestHelper, WsClient, ZapLogger}
+import scala.collection.mutable.ListBuffer
 
 trait ZapTest extends WordSpec {
 
   val logger: Logger = ZapLogger.logger
-  //val alertsToIgnore: List[ZapAlertFilter] = List.empty
   lazy val theClient = new WsClient()
   implicit val zapAlertReads: Reads[ZapAlert] = Json.reads[ZapAlert]
 
   var policyName: String = ""
   var context: Context = _
-  var zapConfig: Config = ConfigFactory.empty()
+  var zapConfig: ZapConfig = new ZapConfig()
 
-  def initialiseConfiguration(config: Config) : Unit = {
-    zapConfig =  new LoadConfig(config).extractedConfig()
+  /*
+   * Call from subclass to inject project specific configuration
+   */
+  def withConfiguration(config: Config) : Unit = {
+    zapConfig =  new ZapConfig(config)
   }
 
   def alertsToIgnore():List[ZapAlertFilter] = {
-    val listofalerts = zapConfig.getConfigList("alertsToIgnore")
-    println ("Printing filter cweid at index 0: " + listofalerts.get(0).getString("cweid"))
-    List.empty
+    val listofalerts: List[Config] = zapConfig.alertsToIgnore
+    val listBuffer: ListBuffer[ZapAlertFilter] = new ListBuffer[ZapAlertFilter]
+
+    listofalerts.foreach { af: Config =>
+      listBuffer.append(ZapAlertFilter(af.getString("cweid"), af.getString("url")))
+    }
+    listBuffer.toList
   }
 
   def callZapApi(queryPath: String, params: (String, String)*): String = {
-    val (status, response) = theClient.get(zapConfig.getString("zapBaseUrl"), queryPath, params: _*)
+    val (status, response) = theClient.get(zapConfig.zapBaseUrl, queryPath, params: _*)
 
     if (status != 200) {
       fail(s"Expected response code is 200, received:$status")
@@ -72,7 +78,7 @@ trait ZapTest extends WordSpec {
     val scannersToDisableForUiTesting = "4,5,42,10000,10001,10013,10014,10022,20000,20001,20002,20003,20004,20005,20006,30001,30002,30003,40000,40001,40002,40006,40010,40011,40018,40020,40022,40027,40028,40029,90001,90029,90030"
     val scannersToEnableForApiTesting = "0,2,3,6,7,42,10010,10011,10012,10015,10016,10017,10019,10020,10021,10023,10024,10025,10026,10027,10032,10040,10045,10048,10095,10105,10202,20012,20014,20015,20016,20017,20018,20019,30001,30002,30003,40003,40008,40009,40012,40013,40014,40016,40017,40018,40019,40020,40021,40022,40023,50000,50001,90001,90011,90019,90020,90021,90022,90023,90024,90025,90026,90028,90029,90030,90033"
 
-    if (!zapConfig.getBoolean("testingAnApi")) {
+    if (!zapConfig.testingAnApi) {
       callZapApi("/json/ascan/action/disableScanners", "ids" -> scannersToDisableForUiTesting, "scanPolicyName" -> policyName)
     }
     else {
@@ -92,15 +98,15 @@ trait ZapTest extends WordSpec {
   }
 
   def setUpContext(contextName: String): Unit = {
-    callZapApi("/json/context/action/includeInContext", "contextName" -> contextName, "regex" -> zapConfig.getString("contextBaseUrl"))
+    callZapApi("/json/context/action/includeInContext", "contextName" -> contextName, "regex" -> zapConfig.contextBaseUrl)
     callZapApi("/json/context/action/excludeAllContextTechnologies", "contextName" -> contextName)
 
-    if (zapConfig.getString("desiredTechnologyNames").nonEmpty) {
-      callZapApi("/json/context/action/includeContextTechnologies", "contextName" -> contextName, "technologyNames" -> zapConfig.getString("desiredTechnologyNames"))
+    if (zapConfig.desiredTechnologyNames.nonEmpty) {
+      callZapApi("/json/context/action/includeContextTechnologies", "contextName" -> contextName, "technologyNames" -> zapConfig.desiredTechnologyNames)
     }
 
-    if (zapConfig.getString("routeToBeIgnoredFromContext").nonEmpty) {
-      callZapApi("/json/context/action/excludeFromContext", "contextName" -> contextName, "regex" -> zapConfig.getString("routeToBeIgnoredFromContext"))
+    if (zapConfig.routeToBeIgnoredFromContext.nonEmpty) {
+      callZapApi("/json/context/action/excludeFromContext", "contextName" -> contextName, "regex" -> zapConfig.routeToBeIgnoredFromContext)
     }
   }
 
@@ -111,22 +117,18 @@ trait ZapTest extends WordSpec {
   }
 
   def runAndCheckStatusOfSpider(contextName: String): Unit = {
-    callZapApi("/json/spider/action/scan", "contextName" -> contextName, "url" -> zapConfig.getString("testUrl"))
+    callZapApi("/json/spider/action/scan", "contextName" -> contextName, "url" -> zapConfig.testUrl)
     TestHelper.waitForCondition(hasCallCompleted("/json/spider/view/status"), "Spider Timed Out", timeoutInSeconds = 600)
   }
 
   def runAndCheckStatusOfActiveScan(contextId: String, policyName: String): Unit = {
-    val isActiveScanRequired = zapConfig.getBoolean("activeScan")
-
-    if (isActiveScanRequired) {
-
-      logger.info(s"Active Scan Config: is set to: $isActiveScanRequired. Triggering Active Scan.")
-
-      callZapApi("/json/ascan/action/scan", "contextId" -> contextId, "scanPolicyName" -> policyName, "url" -> zapConfig.getString("testUrl"))
+    if (zapConfig.activeScan) {
+      logger.info(s"Triggering Active Scan.")
+      callZapApi("/json/ascan/action/scan", "contextId" -> contextId, "scanPolicyName" -> policyName, "url" -> zapConfig.testUrl)
       TestHelper.waitForCondition(hasCallCompleted("/json/ascan/view/status"), "Active Scanner Timed Out", timeoutInSeconds = 1800)
     }
     else
-      logger.info(s"Active Scan Config: is set to: $isActiveScanRequired. Active Scan is NOT triggered.")
+      logger.info(s"Skipping Active Scan")
   }
 
   def writeToFile(report: String): Unit = {
@@ -145,12 +147,11 @@ trait ZapTest extends WordSpec {
   }
 
   def filterAlerts(allAlerts: List[ZapAlert]): List[ZapAlert] = {
-
     val relevantAlerts = allAlerts.filterNot{zapAlert =>
       alertsToIgnore.exists(f => f.matches(zapAlert))
     }
 
-    if(zapConfig.getBoolean("ignoreOptimizelyAlerts"))
+    if(zapConfig.ignoreOptimizelyAlerts)
       relevantAlerts.filterNot(zapAlert => zapAlert.evidence.contains("optimizely"))
     else
       relevantAlerts
@@ -158,7 +159,7 @@ trait ZapTest extends WordSpec {
 
   def testSucceeded(relevantAlerts: List[ZapAlert]): Boolean = {
 
-    val failingAlerts = zapConfig.getString("failureThreshold") match {
+    val failingAlerts = zapConfig.failureThreshold match {
       case "High" => relevantAlerts.filterNot(zapAlert => zapAlert.risk == "Informational" || zapAlert.risk == "Low" || zapAlert.risk == "Medium")
       case "Medium" => relevantAlerts.filterNot(zapAlert => zapAlert.risk == "Informational" || zapAlert.risk == "Low")
       case "Low" => relevantAlerts.filterNot(zapAlert => zapAlert.risk == "Informational")
@@ -169,7 +170,7 @@ trait ZapTest extends WordSpec {
   }
 
   def parsedAlerts: List[ZapAlert] = {
-    val response: String = callZapApi("/json/core/view/alerts", "baseurl" -> zapConfig.getString("alertsBaseUrl"))
+    val response: String = callZapApi("/json/core/view/alerts", "baseurl" -> zapConfig.alertsBaseUrl)
     val jsonResponse = Json.parse(response)
     (jsonResponse \ "alerts").as[List[ZapAlert]]
   }
@@ -195,7 +196,7 @@ trait ZapTest extends WordSpec {
   "Inspecting the alerts" should {
     "not find any unknown alerts" in {
       val relevantAlerts = filterAlerts(parsedAlerts)
-      writeToFile(generateHtmlReport(relevantAlerts.sortBy{ _.severityScore() }, zapConfig.getString("failureThreshold")))
+      writeToFile(generateHtmlReport(relevantAlerts.sortBy{ _.severityScore() }, zapConfig.failureThreshold))
       withClue ("Zap found some new alerts - see above!") {
         assert(testSucceeded(relevantAlerts))
       }
@@ -204,7 +205,7 @@ trait ZapTest extends WordSpec {
 
   "Tearing down the policy, context and alerts" should {
     "complete successfully" in {
-      if(zapConfig.getBoolean("tearDown")){
+      if(zapConfig.debugTearDown){
         logger.debug(s"Removing ZAP Context (${context.name}) Policy ($policyName), and all alerts.")
         tearDown(context.name, policyName)
       } else {
